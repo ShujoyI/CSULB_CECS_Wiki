1aee107a387ace4b5e13313de3be0d81cb54602b
ShujoyI <shujoyi@gmail.com>
2020-11-10 12:25:24 -0800

    Update degree-roadmap.js
    Boilerplate code for degree roadmap graph

--- wiki-prototype-one/src/components/degree-roadmap.js
+++ wiki-prototype-one/src/components/degree-roadmap.js
@@ -1,1 +1,24 @@
 import DagreGraph from "dagre-d3-react"
+
+ReactDOM.render(
+    <div>
+        <DagreGraph
+            nodes={nodes}
+            links={links}
+            options={{
+                rankdir: 'LR',
+                align: 'UL',
+                ranker: 'tight-tree'
+            }}
+            width='500'
+            height='500'
+            animate={1000}
+            shape='circle'
+            fitBoundaries
+            zoomable
+            onNodeClick={e => console.log(e)}
+            onRelationshipClick={e => console.log(e)}
+        />
+    </div>,
+    container
+)
