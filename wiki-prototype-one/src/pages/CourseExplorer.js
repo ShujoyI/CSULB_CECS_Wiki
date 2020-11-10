9f8df61dcacdb3e64052a6ab177f75fb4ee883f0
ShujoyI <shujoyi@gmail.com>
2020-11-10 12:55:25 -0800

    Update CourseExplorer.js
    Display Roadmap on CourseExplorer page

--- wiki-prototype-one/src/pages/CourseExplorer.js
+++ wiki-prototype-one/src/pages/CourseExplorer.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import '../App.css'
+import '../components/DegreeRoadmap'
+import DegreeRoadmap from '../components/DegreeRoadmap';
 
 export default function CourseExplorer() {
     return <h1 className='courseExplorer'> Course Explorer
     </h1>;
+
+    <DegreeRoadmap/>
 }
