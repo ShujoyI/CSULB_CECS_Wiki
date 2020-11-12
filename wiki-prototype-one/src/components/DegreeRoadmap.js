f3929e69c1ba63cfea40916f17eb6c7035815a8b
ShujoyI <shujoyi@gmail.com>
2020-11-12 12:43:26 -0800

    Revert "Update DegreeRoadmap.js"
    This reverts commit 20ada5bdd9748cf59ac34f2f2ac36d837126cbf5.

--- wiki-prototype-one/src/components/DegreeRoadmap.js
+++ wiki-prototype-one/src/components/DegreeRoadmap.js
@@ -1,40 +1,11 @@
 import DagreGraph from "dagre-d3-react"
 
-// test data for graph
-let tempData = {
-    nodes: [
-      {
-        id: "1",
-        label: "<h3>Node 1</h3>",
-        labelType: "html"
-      },
-      {
-        id: "2",
-        label: "<h3>Node 2</h3>",
-        labelType: "html",
-        config: {
-                style: 'fill: #afa'
-            }
-      }
-    ],
-    links: [
-      {
-        source: '1',
-        target: '2',
-        label: 'TO',
-        config: {
-                arrowheadStyle: 'display: none',
-        }
-      },
-    ]
-  }
-
 function DegreeRoadmap(){
     return (
         <div>
         <DagreGraph
-            nodes= {tempData.nodes}
-            links= {tempData.links}
+            nodes={1, 2, 3}
+            links={links}
             options={{
                 rankdir: 'LR',
                 align: 'UL',
