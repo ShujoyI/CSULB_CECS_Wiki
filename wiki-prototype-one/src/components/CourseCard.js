079a5444e84833ed83f7f9fc7b369171e8e393a5
ShujoyI <shujoyi@gmail.com>
2020-11-17 19:18:42 -0800

    Update CourseCard.js
    Called the react-bootstrap stylesheet within CourseCard instead of universally importing it

--- wiki-prototype-one/src/components/CourseCard.js
+++ wiki-prototype-one/src/components/CourseCard.js
@@ -2,8 +2,8 @@ import React from 'react';
 // import "bootstrap/dist/css/bootstrap.min.css";
 import { Card } from 'react-bootstrap';
 import { Button } from './Button'
-// Hardcoded to display CECS 225 Data
 
+// Hardcoded to display CECS 225 Data
 function CourseCard() {
     return (
         <Card>
@@ -21,6 +21,12 @@ function CourseCard() {
             >
                 Prerequisites
             </Button>
+            <link
+            rel="stylesheet"
+            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+            integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+            crossorigin="anonymous"
+            />
         </Card.Body>
         </Card>
     );
