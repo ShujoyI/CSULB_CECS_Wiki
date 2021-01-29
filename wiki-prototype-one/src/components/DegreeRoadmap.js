66333f6c79a646ddae8ba7b83224a9beb106b83b
ShujoyI <shujoyi@gmail.com>
2021-01-28 18:09:09 -0800

    Update DegreeRoadmap.js
    Put course nodes in a separate module called "CourseNodes.js", testing new implementation

--- wiki-prototype-one/src/components/DegreeRoadmap.js
+++ wiki-prototype-one/src/components/DegreeRoadmap.js
@@ -1,6 +1,8 @@
 import React from "react"
 import DagreGraph from "dagre-d3-react"
+import CourseNodes from './CourseNodes.js'
 import './DegreeRoadmap.css'
+import Course from "./Course.js"
 // import PREREQS from "./Prereqs.json"
 // import CourseCard from "./CourseCard"
 
@@ -12,147 +14,149 @@ import './DegreeRoadmap.css'
 
 // test data for graph
 let tempData = {
-  nodes: [
-    
-    {
-      id: "1",
-      label: "<h3>CECS 225</h3>",
-      labelType: "html",
-    },
-    {
-      id: "2",
-      label: "<h3>CECS 228</h3>",
-      labelType: "html",
-    },
-    {
-      id: "3",
-      label: "<h3>CECS 229</h3>",
-      labelType: "html",
-    },
-    {
-      id: "4",
-      label: "<h3>CECS 274</h3>",
-      labelType: "html",
-    },
-    {
-      id: "5",
-      label: "<h3>CECS 277</h3>",
-      labelType: "html",
-    },
-    {
-      id: "6",
-      label: "<h3>CECS 328</h3>",
-      labelType: "html",
-    },
-    {
-      id: "7",
-      label: "<h3>CECS 282</h3>",
-      labelType: "html"
-    },
-    {
-      id: "8",
-      label: "<h3>CECS 341</h3>",
-      labelType: "html"
-    }, 
-    {
-      id: "9",
-      label: "<h3>CECS 329</h3>",
-      labelType: "html"
-    },
-    {
-      id: "10",
-      label: "<h3>CECS 342</h3>",
-      labelType: "html"
-    },
-    {
-      id: "11",
-      label: "<h3>ENGR 350</h3>",
-      labelType: "html"
-    },
-    {
-      id: "12",
-      label: "<h3>CECS 378</h3>",
-      labelType: "html"
-    },
-    {
-      id: "13",
-      label: "<h3>ENGR 361</h3>",
-      labelType: "html"
-    },
-    {
-      id: "14",
-      label: "<h3>CECS 323</h3>",
-      labelType: "html"
-    },
-    {
-      id: "15",
-      label: "<h3>CECS 327</h3>",
-      labelType: "html"
-    },
-    {
-      id: "16",
-      label: "<h3>CECS 343</h3>",
-      labelType: "html"
-    },
-  ],
 
-  links: [
-    {
-      // CECS 225 -> CECS 341
-      source: '1',
-      target: '8',
-    },
-    {
-      // CECS 274 -> CECS 282
-      source: '4',
-      target: '7',
-    },
-    {
-      // CECS 274 -> CECS 328
-      source: '4',
-      target: '6',
-    },
-    {
-      // CECS 328 -> CECS 329
-      source: '6',
-      target: '9',
-    },
-    {
-      // CECS 228 -> CECS 229
-      source: '2',
-      target: '3',
-    },
-    {
-      // CECS 328 -> CECS 342
-      source: '6',
-      target: '10',
-    },
-    {
-      // CECS 228 -> CECS 328
-      source: '2',
-      target: '6',
-    },
-    {
-      // CECS 277 -> CECS 282
-      source: '5',
-      target: '7',
-    },
-    {
-      // CECS 282 -> CECS 343
-      source: '7',
-      target: '16',
-    },
-    {
-      // CECS 228 -> CECS 323
-      source: '7',
-      target: '14',
-    },
-    {
-      // CECS 282 -> CECS 323
-      source: '2',
-      target: '14',
-    },
-  ]
+  // CourseNodes
+  // nodes: [
+    
+  //   {
+  //     id: "1",
+  //     label: "<h3>CECS 225</h3>",
+  //     labelType: "html",
+  //   },
+  //   {
+  //     id: "2",
+  //     label: "<h3>CECS 228</h3>",
+  //     labelType: "html",
+  //   },
+  //   {
+  //     id: "3",
+  //     label: "<h3>CECS 229</h3>",
+  //     labelType: "html",
+  //   },
+  //   {
+  //     id: "4",
+  //     label: "<h3>CECS 274</h3>",
+  //     labelType: "html",
+  //   },
+  //   {
+  //     id: "5",
+  //     label: "<h3>CECS 277</h3>",
+  //     labelType: "html",
+  //   },
+  //   {
+  //     id: "6",
+  //     label: "<h3>CECS 328</h3>",
+  //     labelType: "html",
+  //   },
+  //   {
+  //     id: "7",
+  //     label: "<h3>CECS 282</h3>",
+  //     labelType: "html"
+  //   },
+  //   {
+  //     id: "8",
+  //     label: "<h3>CECS 341</h3>",
+  //     labelType: "html"
+  //   }, 
+  //   {
+  //     id: "9",
+  //     label: "<h3>CECS 329</h3>",
+  //     labelType: "html"
+  //   },
+  //   {
+  //     id: "10",
+  //     label: "<h3>CECS 342</h3>",
+  //     labelType: "html"
+  //   },
+  //   {
+  //     id: "11",
+  //     label: "<h3>ENGR 350</h3>",
+  //     labelType: "html"
+  //   },
+  //   {
+  //     id: "12",
+  //     label: "<h3>CECS 378</h3>",
+  //     labelType: "html"
+  //   },
+  //   {
+  //     id: "13",
+  //     label: "<h3>ENGR 361</h3>",
+  //     labelType: "html"
+  //   },
+  //   {
+  //     id: "14",
+  //     label: "<h3>CECS 323</h3>",
+  //     labelType: "html"
+  //   },
+  //   {
+  //     id: "15",
+  //     label: "<h3>CECS 327</h3>",
+  //     labelType: "html"
+  //   },
+  //   {
+  //     id: "16",
+  //     label: "<h3>CECS 343</h3>",
+  //     labelType: "html"
+  //   },
+  // ],
+
+  // links: [
+  //   {
+  //     // CECS 225 -> CECS 341
+  //     source: '1',
+  //     target: '8',
+  //   },
+  //   {
+  //     // CECS 274 -> CECS 282
+  //     source: '4',
+  //     target: '7',
+  //   },
+  //   {
+  //     // CECS 274 -> CECS 328
+  //     source: '4',
+  //     target: '6',
+  //   },
+  //   {
+  //     // CECS 328 -> CECS 329
+  //     source: '6',
+  //     target: '9',
+  //   },
+  //   {
+  //     // CECS 228 -> CECS 229
+  //     source: '2',
+  //     target: '3',
+  //   },
+  //   {
+  //     // CECS 328 -> CECS 342
+  //     source: '6',
+  //     target: '10',
+  //   },
+  //   {
+  //     // CECS 228 -> CECS 328
+  //     source: '2',
+  //     target: '6',
+  //   },
+  //   {
+  //     // CECS 277 -> CECS 282
+  //     source: '5',
+  //     target: '7',
+  //   },
+  //   {
+  //     // CECS 282 -> CECS 343
+  //     source: '7',
+  //     target: '16',
+  //   },
+  //   {
+  //     // CECS 228 -> CECS 323
+  //     source: '7',
+  //     target: '14',
+  //   },
+  //   {
+  //     // CECS 282 -> CECS 323
+  //     source: '2',
+  //     target: '14',
+  //   },
+  // ]
 
 }
 function DegreeRoadmap(){
@@ -163,8 +167,8 @@ function DegreeRoadmap(){
           <img src="images/img-roadmap.jpg" alt='Travel Image' className="roadMapImage"/>
         </figure> */}
         <DagreGraph
-          nodes={tempData.nodes}
-          links={tempData.links}
+          nodes={CourseNodes.nodes}
+          links={CourseNodes.links}
           config={{
               rankdir: 'LR',
               align: 'DL',
