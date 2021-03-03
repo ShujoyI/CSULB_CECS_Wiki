388f0e80d31a77bc111695999ea983ab31d813e3
ShujoyI <shujoyi@gmail.com>
2021-03-02 19:26:43 -0800

    Update Course.js
    The Course card now allows for a card to render in the Degree Roadmap

--- wiki-prototype-one/src/components/Course.js
+++ wiki-prototype-one/src/components/Course.js
@@ -5,23 +5,44 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function Course(props) {
 
-    if (props.courseDescription === "") {
+    if (props.courseDescription === "" && props.successors === "") {
         return (
             <Card className='myCard'>
                 <Card.Header className='cardHead'>Course Not Selected Yet</Card.Header>
                     <Card.Body className='cardBody'>
-                        <Card.Text claassName='cardText'>Select a class from the list to learn more about it.</Card.Text>
+                        <Card.Text className='cardText'>Select a class from the list to learn more about it.</Card.Text>
                     </Card.Body>
             </Card>
         );
     }
-    else {
+    else if (props.successors === "") {
         return (
             <Card className='myCard'>
                 <Card.Header className='cardHead'>Course Info</Card.Header>
                     <Card.Body className='cardBody'>
                         <Card.Title className='cardTitle'>{props.courseNumber}</Card.Title>
                         <Card.Text className='cardText'>{props.courseDescription}</Card.Text>
+                    </Card.Body>
+            </Card>
+        );
+    }
+
+    else {
+        const successors = props.successors;
+        return (
+            <Card className='myCard'>
+                <Card.Header className='cardHead'>Course Prerequisites</Card.Header>
+                    <Card.Body className='cardBody'>
+                        <Card.Title className='cardTitle'>{props.courseNumber}</Card.Title>
+                        <Card.Text className='cardText'> Courses you can take after are:
+                            <p>{
+                                successors.map((successor, index) => (
+                                    <li key={index}>
+                                        {successor}
+                                    </li>
+                                ))}
+                            </p>
+                        </Card.Text>
                     </Card.Body>
             </Card>
         );
