27a8c29d5f80eb3b7e50fb87d20e70a8912d0513
dmg59912 <dmg138380@yahoo.com>
2021-03-04 19:18:44 -0800

    funtion sum

--- wiki-prototype-one/src/pages/Possible_IQ.js
+++ wiki-prototype-one/src/pages/Possible_IQ.js
@@ -9,8 +9,15 @@ const Possible_IQ = () =>
     return (
     <div>
         <h3>
-            Welcome Interview Questions!
+            Welcome Interview Question.
         </h3>
+        <h3>Lets start with a warm up!</h3>
+        <p>Write a function that returns the sum of two numbers.</p>
+        <p>Ex. value1 = 6 and value2 = 9</p>
+        <p>Output:15 </p>
+        <p>int sum( int value1, int value2)
+            
+        </p>
 
         <small>It is game Time </small>
         
