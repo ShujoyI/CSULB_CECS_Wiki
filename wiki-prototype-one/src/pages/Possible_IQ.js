f0896bf97d61c4ecc6a3f52184a3a71eedbbb534
dmg59912 <dmg138380@yahoo.com>
2021-03-09 18:14:13 -0800

    Added one question

--- wiki-prototype-one/src/pages/Possible_IQ.js
+++ wiki-prototype-one/src/pages/Possible_IQ.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import '../styles/Possible_IQ.css';
 import {Link} from "react-router-dom";
 
 
@@ -11,6 +12,8 @@ const Possible_IQ = () =>
         <h3>
             Welcome Interview Question.
         </h3>
+
+        <div>
         <h3>Lets start with a warm up!</h3>
         <p>Write a function that returns the sum of two numbers.</p>
         <p>Ex. value1 = 6 and value2 = 9</p>
@@ -21,6 +24,34 @@ const Possible_IQ = () =>
 
         <small>It is game Time </small>
         
+       
+        </div>
+
+        <div className='question1'>
+            <h3>Question 1</h3>
+            <p>Given an array a that contains only numbers in the range from 1 to a.length</p>
+            <p>find the first duplicate number for which the second occurence has the minimal index.</p>
+            <p>If there is not such elements, return -1.</p>
+            <h4>int first_duplicate(int array_a) </h4>
+            <ul>
+                <h3>Examples </h3>
+                <li>
+                    For a = [8,4,6,2,6,4,7]     Output = 6
+                </li>
+                <li>
+                    For a = [3,4,1,9]    Output = -1
+                </li>
+            </ul>
+      
+        
+        <ul>
+            <h3>Constrains </h3>
+            <li>Should have a O(n) time </li>
+            <li>Requieres only O(1) space complexity </li>
+        </ul>
+
+        </div>
+
         <div>
             <Link to='/guides'>Back to Guide</Link>
         </div>
