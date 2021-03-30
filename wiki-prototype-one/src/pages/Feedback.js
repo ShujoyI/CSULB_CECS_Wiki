c3cd6c61f770760fe63bd1a3a3a5aed691d75a27
aram249 <55257824+aram249@users.noreply.github.com>
2021-03-29 20:52:03 -0700

    3/29/2021
    Feedback implementation complete

--- wiki-prototype-one/src/pages/Feedback.js
+++ wiki-prototype-one/src/pages/Feedback.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React from 'react';
 import '../App.css'
 import '../styles/Feedback.css';
-import { Link } from 'react-router-dom';
 
-export default function Feedback() {
+export default function Feedback(){ 
     return (
         <div className='feedback'>
             <div className = 'feedback-container'>
                 <p></p>
                 <p><h1 id="feedback-"><b></b></h1></p>
-                <p>Sorry! Feedback page is still under construction. Will be done by the end of the week.</p>
+                <iframe
+                    title="Feedback and Q&A"
+                    src="https://docs.google.com/forms/d/e/1FAIpQLSeCwdc-M450A535j2TGHuuZcuUb8MjF_Q7ziW8iQ9pjcbZIXg/viewform?embedded=true"
+                    width="100%"
+                    height="800"
+                    frameBorder="0"
+                    marginHeight="0"
+                    marginWidth="0"
+                >
+                    Loading...
+                </iframe>
             </div>
         </div>
     );
+    
 }
