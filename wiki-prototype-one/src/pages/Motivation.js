18df64cfb68eaa6cb41a61e18023d7fda46d07f3
dmg59912 <dmg138380@yahoo.com>
2021-04-13 19:28:40 -0700

    added a few videos

--- wiki-prototype-one/src/pages/Motivation.js
+++ wiki-prototype-one/src/pages/Motivation.js
@@ -11,7 +11,41 @@ const Motivation = () =>
             Welcome to Motivation Guides!
         </h3>
 
-        <small>Winning is what's up!!!</small>
+        <div className = "motivation_container">
+            <div className="row">
+                <div className="video">
+                    <div>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/Ks-_Mh1QhMc" title="YouTube video player" frameborder="0" allow="accelerometer; 
+                    autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+
+
+                </div>
+
+                <div className= "video">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/RcGyVTAoXEU" title="YouTube video player" frameborder="0" allow="accelerometer; 
+                autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+
+                <div className="video">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/2FMBSblpcrc" title="YouTube video player" frameborder="0" allow="accelerometer; 
+                autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+
+                <div className="video">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/n3kNlFMXslo" title="YouTube video player" frameborder="0" allow="accelerometer; 
+                autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+
+                <div className="video">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/VSceuiPBpxY" title="YouTube video player" frameborder="0" allow="accelerometer; 
+                autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+                </div>
+
+            </div>
+
+        </div>
         <div>
             <Link to='/guides'>Back to Guide</Link>
         </div>
