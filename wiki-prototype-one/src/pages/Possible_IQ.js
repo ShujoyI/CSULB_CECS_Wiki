87c25d01a637f705abc718a2439f213b8dc8d4a9
dmg59912 <dmg138380@yahoo.com>
2021-04-20 17:54:56 -0700

    added a few more questions

--- wiki-prototype-one/src/pages/Possible_IQ.js
+++ wiki-prototype-one/src/pages/Possible_IQ.js
@@ -27,7 +27,7 @@ const Possible_IQ = () =>
        
         </div>
 
-        <div className='question1'>
+        <div className='questions'>
             <h3>Question 1</h3>
             <p>Given an array a that contains only numbers in the range from 1 to a.length</p>
             <p>find the first duplicate number for which the second occurence has the minimal index.</p>
@@ -52,8 +52,57 @@ const Possible_IQ = () =>
 
         </div>
 
+        <div className = 'questions'>
+            <h3>Question 2</h3>
+            <p> Given a string s of lower case English letters,</p>
+            <p>Find and return the first instance of a non-repeating character in it,</p>
+            <p>If there is no repeating charecter, return '_'</p>
+            <h4>char first_non_reapeating_char(std::string s) </h4>
+            <ul>
+                <h3>Examples</h3>
+                <li>
+                    For s = "abarcusb"      Output = 'r'
+                </li>
+                <li>
+                    For s = "csulbcsulb"    Output = '_'
+                </li>
+            </ul>
+
+            <ul>
+            <h3>Constrains </h3>
+            <li>Should have a O(n) time </li>
+            <li>Requieres only O(1) space complexity </li>
+        </ul>
+        </div>
+
+        <div className = 'questions'>
+            <h3>Question 3</h3>
+            <p> Given a n x n 2d Matrix,</p>
+            <p>Rotate the image by 90 degrees (clockwise)</p>
+            <h4>void rotate( matrix[][]) </h4>
+            <ul>
+                <h3>Examples</h3>
+                <li>
+                    For a<br/> [[1,2,3],]<br/>  
+                            [4,5,6],<br/>
+                            [7,8,8]]<br/><br/>
+                    Output <br/>
+                    [[7,4,1,],<br/>
+                    [8,5,2],<br/>
+                    [9,6,3]]
+
+                </li>
+            </ul>
+
+            <ul>
+                <h3>Constrains </h3>
+                <li>Should have a O(n) time </li>
+                <li>Requieres only O(1) space complexity </li>
+            </ul>
+        </div>
+
         <div>
-            <Link to='/guides'>Back to Guide</Link>
+            <h2><Link to='/guides'>Back to Guide</Link></h2>
         </div>
     </div>
     );
