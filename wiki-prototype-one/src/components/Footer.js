fa7195839a644499cef3aa72c26e646ad1479abb
lifeofparth <chhasatiyaparth@gmail.com>
2021-05-04 09:14:52 -0700

    Footer & Sitemap
    Footer scales properly and sitemap is updated

--- wiki-prototype-one/src/components/Footer.js
+++ wiki-prototype-one/src/components/Footer.js
@@ -1,34 +1,29 @@
 import React from 'react'
 import '../styles/Footer.css';
-import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
     <footer classname="footer">
-      {/* <div class="inner-footer">
-        <div class="quick-links">
-          <ul>
-            <li class="quick-items"><a href="/about">About</a></li>
-            <li class="quick-items"><a href="/contact">Contact</a></li>
-            <li class="quick-items"><a href="/sitemap">Sitemap</a></li>
-            <li class="quick-items"><a href="/feedback">Feedback</a></li>
-            
-          </ul>
-        </div>
-      </div> */}
       <div class="outer-footer">
         <div class="quick-links">
             <ul>
-              <li class="quick-items"><a href="/about">About</a></li>
-              <li class="quick-items"><a href="mailto:beachCSWiki@gmail.com">Contact</a></li>
-              <li class="quick-items"><a href="/sitemap">Sitemap</a></li>
-              <li class="quick-items"><a href="/feedback">Feedback</a></li>
-              
+                <li class="quick-items">
+                    <a href="/about">About</a>
+                </li>
+                <li class="quick-items">
+                    <a href="mailto:beachCSWiki@gmail.com">Contact</a>
+                </li>
+                <li class="quick-items">
+                    <a href="/sitemap">Sitemap</a>
+                </li>
+                <li class="quick-items">
+                    <a href="/feedback">Feedback</a>
+                </li>
             </ul>
         </div>
-        &copy; {(new Date().getFullYear())} Copyright: <a href="https://www.beachcs.wiki/"> BeachCS </a>
-        
-      
+        <div className="copyright">
+            &copy; {(new Date().getFullYear())} Copyright: <a href="https://www.beachcs.wiki/"> BeachCS </a>
+        </div>
       </div>
     </footer>
   );
